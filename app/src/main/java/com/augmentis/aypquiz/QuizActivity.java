a5446a369a5b5972c34a5516b9b9c09910d3e762
Tanaphon <tanaphon.a@augmentis.biz>
2016-07-15 13:09:46 +0700

    Android Lesson 2 Add land scape

--- app/src/main/java/com/augmentis/aypquiz/QuizActivity.java
+++ app/src/main/java/com/augmentis/aypquiz/QuizActivity.java
@@ -10,6 +10,9 @@ import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
+    private static final String TAG = "AYPQUIZ";
+    private static final String INDEX = "INDEX";
+
     Button trueButton;
     Button falseButton;
 
@@ -26,19 +29,58 @@ public class QuizActivity extends AppCompatActivity {
 
     int currentIndex;
 
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "On Start");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "On Pause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "On Resume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "On Stop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "On Destroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "Stage is saving");
+        outState.putInt(INDEX, currentIndex);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "On Create");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
-
-
         trueButton = (Button) findViewById(R.id.true_button);
         falseButton = (Button) findViewById(R.id.false_button);
         nextButton = (Button) findViewById(R.id.next_button);
         previousButton = (Button) findViewById(R.id.previous_button);
         questionText = (TextView) findViewById(R.id.text_question);
-        currentIndex = 0;
+        if(savedInstanceState != null) currentIndex = savedInstanceState.getInt(INDEX);
+        else currentIndex = 0;
         questionText.setText(questions[currentIndex].getQuestionId());
+
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -79,6 +121,7 @@ public class QuizActivity extends AppCompatActivity {
                 checkAnswer(false);
             }
         });
+
     }
 
     public void checkAnswer(boolean answer) {
